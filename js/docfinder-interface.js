c481694f3d2698e57641ae19012b80f931754171
Mara Timberlake <maratimberlake@msn.com>
2017-08-04 22:46:41 -0700

    change interface layout

--- js/docfinder-interface.js
+++ js/docfinder-interface.js
@@ -1,25 +1,12 @@
 var DoctorFinder = require('./../js/docfinder.js').doctorModule;
 
-var getCount = function(location, count) {
-  $('#portland-doctors').text(count);
-};
-
-var getDoctorStats = function(medical-issue, listing) {
-  // listings.forEach(function(element) {
-    $('#doctor-specialty').append('<li>'+listing.specialty+'</li>');
-    $('#location').append('<li>'+listing.location+'</li>');
-    $('#insurance-plans').append('<li>'+convertDate(listing.insurance_plans)+'</li>');
-};
-
 $(document).ready(function() {
   var newDoctorFinder = new DoctorFinder();
-  var location = "Portland";
-  newDoctorFinder.getDoctors(location, getCount);
 
   $('#medical-issue-form').submit(function(event) {
     event.preventDefault();
-    var medical-issue = $('#medical-issue').val();
+    var medicalIssue = $('#medical-issue').val();
     $('#medical-issue').empty();
-    newDoctorFinder.getDoctorsByMedicalIssue(medical-issue, getDoctorStats);
+    newDoctorFinder.getDoctors(medicalIssue);
   });
 });
--- js/docfinder.js
+++ js/docfinder.js
@@ -1,39 +1,37 @@
-var apiKey = require('./../.env').apiKey;
+var apiKey = require('../.env').apiKey;
 
 function DoctorFinder() {
 
 }
 
-DoctorFinder.prototype.getDoctors = function(location, getCount) {
-  $.get('https://doctorindex.org:443/api/v3/search/count?location='+location+'&distance=10&stolenness=stolen').then(function(response) {
-      getCount(location, response.proximity);
-      console.log(JSON.stringify(response));
-    }).fail(function(error) {
-      $('.portland-doctors').text(error.responseJSON.message);
-    });
-  };
-
-exports.getDoctors = functionq(medicalIssue) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
-   .then(function(result) {
-      console.log(result);
-    })
-   .fail(function(error){
-      console.log("fail");
-    });
-  };
-};
-
-
-// DoctorFinder.prototype.getDoctorsByMedicalIssue = function(medicalIssue, getDoctorStats) {
-//   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey).then(function(response) {
-//
-//     response.doctors.forEach(function(doctor) {
-//       getDoctorStats(medical-issue, doctor);
-//       console.log(doctor);
-//     });
-//
+// DoctorFinder.prototype.getDoctors = function(location, getCount) {
+//   $.get('https://doctorindex...').then(function(response) {
+//     getCount(location, response.proximity);
+//     console.log(JSON.stringify(response));
 //   }).fail(function(error) {
-//     $('.medical-issue-results').text(error.responseJSON.message);
+//     $('.portland-doctors').text(error.responseJSON.message);
 //   });
 // };
+
+DoctorFinder.prototype.getDoctors = function(medicalIssue) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+  .then(function(result) {
+     console.log(result);
+     var list = $('#doctor-list');
+     result['data'].forEach(function(item) {
+       var profile = item['profile'];
+       list.append('<li>'
+       + profile['first_name']
+       + ' '
+       + profile['last_name']
+       + ', '
+       + profile['title']
+       + '</li>');
+     });
+   })
+  .fail(function(error){
+     console.log("fail");
+   });
+};
+
+exports.doctorModule = DoctorFinder;
